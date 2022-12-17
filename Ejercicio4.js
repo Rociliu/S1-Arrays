a9b61488055e4785c9e09619bf9c037fa655562b
Rociliu <mrrosa.carrasco@gmail.com>
2022-12-17 13:15:16 +0100

    Ejercicio4

---
+++ Ejercicio4.js
@@ -0,0 +1,7 @@
+"use strict";
+//Añade 2 elementos al array: "Morty" y "Summer". Muestra en consola el último personaje del array
+
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+rickAndMortyCharacters.push("Morty", "Summer");
+console.log(rickAndMortyCharacters);
+console.log(rickAndMortyCharacters[rickAndMortyCharacters.length-1]);
